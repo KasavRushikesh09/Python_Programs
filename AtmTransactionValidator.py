12bc15405c9c9f574323e476fd16abba050d64ac
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-01-07 14:40:07 +0530

    atm transaction validator problem

---
+++ AtmTransactionValidator.py
@@ -0,0 +1,14 @@
+balance = int(input())
+
+n = int(input())
+
+for _ in range(n):
+    amount = int(input())
+    if amount%100 == 0 or balance >= amount:
+        print("Success")
+        balance -= amount
+
+
+    else:
+        print("Failed")
+print(balance)

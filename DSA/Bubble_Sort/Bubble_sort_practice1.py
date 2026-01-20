5caeeb9581808a75dbf789154bf78c17bbd616db
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-01-20 23:35:02 +0530

    Bubble sort array sort

--- DSA/Bubble_Sort/Bubble_sort_practice1.py
+++ DSA/Bubble_Sort/Bubble_sort_practice1.py
@@ -1,1 +1,13 @@
 #Practice_1
+arr = [5,1,4,2,8]
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
+
+result = bubble_sort(arr)
+print(result)

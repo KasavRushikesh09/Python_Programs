1d4469790d01e304b4beef5578432e54e01a72d8
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-01-21 00:12:49 +0530

    Check if array is already sorted

---
+++ DSA/Bubble_Sort/check_sorted.py
@@ -0,0 +1,27 @@
+# list = [7,2,3,4]
+#
+# def sorted_func(list):
+#     n = len(list)
+#     for i in range(n):
+#         for j in (0,n-i-1):
+#             if list[j] > list[j+1]:
+#                 list[j],list[j+1] = list[j+1],list[j]
+#     return list
+# res = sorted_func(list)
+# print(res)
+arr = [1,9,3,4,5,6]
+
+def bubble_sort(arr):
+    n = len(arr)
+    swap = False
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                swap = True
+                break
+
+    if not swap:
+        return "Already Sorted"
+    else:
+        return "Not Sorted"
+print(bubble_sort(arr))
